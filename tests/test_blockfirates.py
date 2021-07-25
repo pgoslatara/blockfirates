811a2827d1721bc2b58e6354236e7ac57a346a33
P.G. O Slatara <pgoslatara@gmail.com>
2021-07-25 14:38:01 +0200

    Initial commit.

---
+++ tests/test_blockfirates.py
@@ -0,0 +1,18 @@
+from blockfirates import __version__
+from blockfirates.client import BlockFiRates
+
+
+def test_version():
+    assert __version__ == '0.2.0'
+
+
+def test_rates_returned():
+    assert len(BlockFiRates().get_all_rates()) > 0
+
+
+def test_get_amount():
+    assert BlockFiRates().get_amount("BTC (Tier 1)")
+
+
+def test_get_apy():
+    assert BlockFiRates().get_apy("BTC (Tier 1)") > 0
